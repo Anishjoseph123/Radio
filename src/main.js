ae0b4c4c03c5b11e09c9f71805134ed9d512a17b
Anish Joseph <46836583+Anishjoseph123@users.noreply.github.com>
2024-03-20 15:11:39 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,14 +9,14 @@ Vue.config.productionTip = false;
 
 Amplify.configure({
   Auth: {
-    userPoolId: "ap-south-1_8nDuyw57o",
+    userPoolId: "xxxxxxxxxxxx",
     region: "ap-south-1",
-    userPoolWebClientId: "lkc6nedpiieoql4bcs64j8icc",
+    userPoolWebClientId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
   },
   API: {
     aws_project_region: "ap-south-1",
     aws_appsync_graphqlEndpoint:
-      "https://4iz64aofavb55atgx54kfzn6mu.appsync-api.ap-south-1.amazonaws.com/graphql",
+      "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     aws_appsync_region: "ap-south-1",
     aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
   },
@@ -25,7 +25,7 @@ Amplify.configure({
 import * as VueGoogleMaps from "vue2-google-maps";
 Vue.use(VueGoogleMaps, {
   load: {
-    key: "AIzaSyBUp8E7OghfEFg9fJ1ePokoOhE1I8MaD-M",
+    key: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     libraries: "places",
   },
 });
